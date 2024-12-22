b0057306a4eaaeaa9a38e887d9919a275f205b13
Eric Fitzgerald <github@efitz.net>
2024-12-21 22:57:21 -0500

    linting and error handling

--- r53.py
+++ r53.py
@@ -1,10 +1,46 @@
 #!/usr/bin/env python3
+
+"""
+r53.py
+
+This script is a command-line tool for managing AWS Route 53 DNS records.
+
+It allows users to perform various operations on Route 53 hosted zones
+and resource records using the AWS API.
+
+Features:
+- List all hosted zones in your AWS account.
+- List all resource records in a specified hosted zone.
+- Retrieve details of a specific resource record in a hosted zone.
+- Add or update resource records in a hosted zone.
+- Delete a specific resource record from a hosted zone.
+- Update a DNS record with the public IP address of the host running the script
+  or an EC2 instance's public IP address.
+
+Dependencies:
+- boto3: AWS SDK for Python
+- argparse: For parsing command-line arguments
+- re: Regular expressions for pattern matching
+- socket: For network-related operations
+- urllib: For making HTTP requests
+- sys: For system-specific parameters and functions
+- logging: For logging messages
+
+Usage:
+    python r53.py [options]
+
+Author:
+    Eric Fitzgerald (https://github.com/ericfitz)
+
+"""
+
 import argparse
 import re
 import socket
-import urllib.request as request
-import boto3
+from urllib import request, error
+import sys
 import logging
+import boto3
 
 # set up logging
 logger = logging.getLogger(__name__)
@@ -17,24 +53,24 @@ def get_instance_ip(instance_id):
     """
     try:
         response = ec2.describe_instances(InstanceIds=[instance_id])
-    except Exception as e:
-        logger.error("AWS ec2-escribe-instances error: {}".format(e))
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS ec2-describe-instances error: %s", e)
+        sys.exit()
     # return the first public IP address found
     for r in response["Reservations"]:
         for i in r["Instances"]:
             return i["PublicIpAddress"]
 
 
-def get_ip_from_eip(eip):
+def get_ip_from_eip(eip_allocation_id):
     """
     Given an Elastic IP Allocation Id, returns the public IP address of that Elastic IP address.
     """
     try:
-        response = ec2.describe_addresses(AllocationIds=[eip])
-    except Exception as e:
-        logger.error("AWS ec2-describe-addresses error: {}".format(e))
-        exit(1)
+        response = ec2.describe_addresses(AllocationIds=[eip_allocation_id])
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS ec2-describe-addresses error: %s", e)
+        sys.exit()
     # return the first public IP address found
     for a in response["Addresses"]:
         return a["PublicIp"]
@@ -49,9 +85,12 @@ def get_my_ip():
     Returns:
         str: the public IP address of this host.
     """
-    with request.urlopen("https://checkip.amazonaws.com") as f:
-        return f.read().decode("utf-8").strip()
-    # todo: error handling
+    try:
+        with request.urlopen("https://checkip.amazonaws.com") as f:
+            return f.read().decode("utf-8").strip()
+    except (error.URLError, error.HTTPError, socket.error) as e:
+        logger.error("Error retrieving public IP address: %s", e)
+        sys.exit()
 
 
 # https://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python
@@ -59,7 +98,8 @@ def is_valid_ipv4_address(address):
     """
     Validate an IP address.
 
-    Given a string, this function checks if that string is a valid IPv4 address in dotted quad format.
+    Given a string, this function checks if that string is a valid IPv4 address
+    in dotted quad format.
 
     :param address: a string to be validated as an IPv4 address
     :return: True if the address is valid, False otherwise
@@ -174,14 +214,16 @@ def get_hosted_zone_id_from_name(p_domain_name):
             zones = response["HostedZones"]
             for zone in zones:
                 l_zone_id = (zone["Id"].split("/")[-1:])[0]
-                # split by / into a list of strings, get list of last string only, convert list to string
-                zone_name = zone["Name"].rstrip(".")  # remove trailing .
-                if zone_name == p_domain_name:
+                # 1. split by / into a list of strings
+                # 2. get list of last string only
+                # 3. convert list to string
+                current_zone_name = zone["Name"].rstrip(".")  # remove trailing .
+                if current_zone_name == p_domain_name:
                     return l_zone_id
         return None
-    except Exception as e:
-        logger.error("AWS route53-list-hosted-zones error: {}".format(e))
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS route53-list-hosted-zones error: %s", e)
+        sys.exit()
 
 
 def list_hosted_zones():
@@ -203,9 +245,9 @@ def list_hosted_zones():
                 l_zone_name = zone["Name"].rstrip(".")
                 print(l_zone_id, l_zone_name)
         return None
-    except Exception as e:
-        logger.error("AWS route53-list-hosted-zones error: {}".format(e))
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS route53-list-hosted-zones error: %s", e)
+        sys.exit()
 
 
 def get_current_record(p_zone_id, p_record_name):
@@ -238,9 +280,9 @@ def get_current_record(p_zone_id, p_record_name):
                         result["Type"] = rrset["Type"]
                         result["TTL"] = rrset["TTL"]
                         result["Value"] = rr["Value"]
-    except Exception as e:
-        logger.error("AWS route53-list-resource-record-sets error: {}".format(e))
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS route53-list-resource-record-sets error: %s", e)
+        sys.exit()
     return result
 
 
@@ -270,17 +312,17 @@ def list_rr(p_zone_id, p_record_name):
                     rrs = rrset["ResourceRecords"]
                     for rr in rrs:
                         print(
-                            "Name: {}, Type: {}, TTL: {}, Value: {}".format(
-                                rrset_name, rrset["Type"], rrset["TTL"], rr["Value"]
-                            )
+                            f"Name: {rrset_name}, Type: {rrset["Type"]}, TTL: {rrset["TTL"]}, Value: {rr["Value"]}"
                         )
                 except (
                     KeyError
                 ):  # A records for ELB have an odd format, just print the JSON for them
-                    print(rrset)
-    except Exception as e:
-        logger.error("AWS route53-list-resource-record-sets error: {}".format(e))
-        exit(1)
+                    logger.error(
+                        "AWS route53-get-record-set returned unexpected json %s", rrset
+                    )
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS route53-list-resource-record-sets error: %s", e)
+        sys.exit()
     return None
 
 
@@ -322,9 +364,9 @@ def change_rr(p_action, p_zone_id, p_record_type, p_record_name, p_value, p_ttl)
                 ],
             },
         )
-    except Exception as e:
-        logger.error("AWS route53-change-resource-record-sets error: {}".format(e))
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("AWS route53-change-resource-record-sets error: %s", e)
+        sys.exit()
     return response
 
 
@@ -385,158 +427,174 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-logger.debug(f"Arguments: {str(args)}")
+logger.debug("Arguments: %s", str(args))
 
 # set up the boto3 clients
 if args.profile is not None:
-    logger.info(f"Using AWS profile: {args.profile}")
+    logger.info("Using AWS profile: %s", args.profile)
     try:
         boto3.setup_default_session(profile_name=args.profile)
-    except Exception as e:
+    except boto3.exceptions.Boto3Error as e:
         logger.error(
-            f"Boto error {e} creating session using specified profile {args.profile}"
+            "Boto error %s creating session using specified profile %s", e, args.profile
         )
-        exit(1)
+        sys.exit()
 
 # AWS Route 53 is global, not regional, so we can ignore region for Route 53 connection.
 try:
     route53 = boto3.client("route53")
-except Exception as e:
-    logger.error(f"Boto error {e} creating Route 53 client")
-    exit(1)
+except boto3.exceptions.Boto3Error as e:
+    logger.error("Boto error %s creating Route 53 client", e)
+    sys.exit()
 
-# EC2 is regional, so we need to use region if specified (we use EC2 to look up instance IP addresses)
+# EC2 is regional, so we need to use region if specified
+# r53 uses EC2 to look up instance IP addresses & EIPs
 if args.region is not None:
-    logger.info(f"Using AWS region: {args.region}")
+    logger.info("Using AWS region: %s", args.region)
     try:
         ec2 = boto3.client("ec2", region_name=args.region)
-    except Exception as e:
+    except boto3.exceptions.Boto3Error as e:
         logger.error(
-            f"Boto error {e} creating EC2 client in specified region {args.region}"
+            "Boto error %s creating EC2 client in specified region %s", e, args.region
         )
-        exit(1)
+        sys.exit()
 else:
     try:
         ec2 = boto3.client("ec2")
-    except Exception as e:
-        logger.error(f"Boto error {e} creating EC2 client in default region")
-        exit(1)
+    except boto3.exceptions.Boto3Error as e:
+        logger.error("Boto error %s creating EC2 client in default region", e)
+        sys.exit()
 
 # Validate and process the provided parameters
-zone_name = args.zone
-record_name = args.name
-record_type = args.type
+ZONE_NAME = args.zone
+RECORD_NAME = args.name
+RECORD_TYPE = args.type
 value = args.value
 eip = args.eip
 myip = args.myip
 instanceid = args.instanceid
+ttl = args.ttl
 
 # only allow one of (value, eip, myip, instanceid) to be specified
-numvalues = 0
+NUMVALUES = 0
 
 if value is not None:
-    numvalues += 1
+    NUMVALUES += 1
 
 if eip is not None:
-    numvalues += 1
+    NUMVALUES += 1
     value = get_ip_from_eip(args.eip)
-    logger.debug(f"Calculated value from eip: {value}")
+    logger.debug("Calculated value from eip: %s", value)
 
 if myip:
-    numvalues += 1
+    NUMVALUES += 1
     value = get_my_ip()
-    logger.debug(f"Calculated value from IP lookup: {value}")
+    logger.debug("Calculated value from IP lookup: %s", value)
 
 if instanceid is not None:
-    numvalues += 1
+    NUMVALUES += 1
     value = get_instance_ip(args.instanceid)
-    logger.debug(f"Calculated value from EC2 instance ID: {value}")
+    logger.debug("Calculated value from EC2 instance ID: %s", value)
 
-if numvalues > 1:
+if NUMVALUES > 1:
     raise ValueError(
         "Specify only one of value, eip, myip, or instanceid"
     )  # only one of value, eip, myip, or instanceid can be specified
-    exit(1)
 
 # figure out the record type if not explicitly specified
-if record_type is None:
+if RECORD_TYPE is None:
     if is_valid_ipv4_address(value):
-        record_type = "A"
+        RECORD_TYPE = "A"
     elif is_valid_ipv6_address(value):
-        record_type = "AAAA"
+        RECORD_TYPE = "AAAA"
     elif is_valid_dns_name(value):
-        record_type = "CNAME"
-    logger.info(f"Inferred record type: {record_type}")
+        RECORD_TYPE = "CNAME"
+    logger.info("Inferred record type: %s", RECORD_TYPE)
 
 # look up zone id if zone name provided
-zone_id = None
-if zone_name is not None:
+ZONE_ID = None
+if ZONE_NAME is not None:
     if is_valid_dns_name(args.zone) is False:
-        raise ValueError("Invalid zone name: {}".format(args.zone))
-    zone_id = get_hosted_zone_id_from_name(args.zone)
-    logger.debug(f"Matched zone name {zone_name} to zone ID {zone_id}")
+        raise ValueError("Invalid zone name: %s" % args.zone)
+    ZONE_ID = get_hosted_zone_id_from_name(args.zone)
+    logger.debug("Matched zone name %s to zone ID %s", ZONE_NAME, ZONE_ID)
 
 # append the zone name to the record name if required
-if record_name is not None and zone_name is not None:
-    record_name = str(args.name) + "." + str(args.zone)
-    logger.debug(f"Concatenated record name: {record_name}")
+if RECORD_NAME is not None and ZONE_NAME is not None:
+    RECORD_NAME = str(args.name) + "." + str(args.zone)
+    logger.debug("Concatenated record name: %s", RECORD_NAME)
 
 # figure out the action to take
-if zone_id is None:
-    action = "LISTZONES"
+# logic is described in Google sheet: https://bit.ly/r53params
+if ZONE_ID is None:
+    ACTION = "LISTZONES"
 else:
-    if record_name is None:
-        action = "LIST"
+    if RECORD_NAME is None:
+        ACTION = "LIST"
     else:
-        if record_type is None:
-            action = "DESCRIBE"
+        if RECORD_TYPE is None:
+            ACTION = "DESCRIBE"
         else:
             if value is None:
                 if args.delete:
-                    action = "DELETE"
+                    ACTION = "DELETE"
                 else:
                     raise ValueError("Must specify value for upserts")
             else:
-                action = "UPSERT"
+                ACTION = "UPSERT"
 
-if action is None:
+if ACTION is None:
     raise ValueError(
         "Invalid parameter combination and/or values"
     )  # if we got here, there was a bug in the parameter validation code above
 else:
-    logger.info("Inferred action: {}".format(action))
+    logger.info("Inferred action: %s", ACTION)
 
 # execute the action
-match action:
+match ACTION:
     case "LISTZONES":
-        logger.debug(f"Executing action: action:{action}")
+        logger.debug("Executing action: action:%s", ACTION)
         list_hosted_zones()
     case "LIST":
-        logger.debug(f"Executing action: action:{action} zoneid:{zone_id}")
-        list_rr(zone_id, ".")
+        logger.debug("Executing action: action:%s zoneid:%s", ACTION, ZONE_ID)
+        list_rr(ZONE_ID, ".")
     case "DESCRIBE":
         logger.debug(
-            f"Executing action: action:{action} zoneid:{zone_id} recordname:{record_name}"
+            "Executing action: action:%s zoneid:%s recordname:%s",
+            ACTION,
+            ZONE_ID,
+            RECORD_NAME,
         )
-        list_rr(zone_id, record_name)
+        list_rr(ZONE_ID, RECORD_NAME)
     case "UPSERT":
         logger.debug(
-            f"Executing action: action:{action} zoneid:{zone_id} recordname:{record_name} value:{value} ttl:{ttl}"  # type: ignore
+            "Executing action: action:%s zoneid:%s recordname:%s value:%s ttl:%s",
+            ACTION,
+            ZONE_ID,
+            RECORD_NAME,
+            value,
+            args.ttl,
         )
-        change_rr(action, zone_id, record_type, record_name, value, args.ttl)
+        change_rr(ACTION, ZONE_ID, RECORD_TYPE, RECORD_NAME, value, args.ttl)
     case "DELETE":
+        current_record = get_current_record(ZONE_ID, RECORD_NAME)
         logger.debug(
-            f"Executing action: action:{action} zoneid:{zone_id} recordname:{record_name} value:{value} ttl:{ttl}"  # type: ignore
+            "Executing action: action:%s zoneid:%s recordname:%s value:%s ttl:%s",
+            ACTION,
+            ZONE_ID,
+            RECORD_NAME,
+            value,
+            current_record["TTL"],
         )
-        current_record = get_current_record(zone_id, record_name)
-        ttl = current_record["TTL"]
         value = current_record["Value"]
         logger.debug("Validated current record exists before attempting delete")
         try:
-            change_rr(action, zone_id, record_type, record_name, value, args.ttl)
+            change_rr(
+                ACTION, ZONE_ID, RECORD_TYPE, RECORD_NAME, value, current_record["TTL"]
+            )
         except KeyError:  # trying to delete nonexistent record
             logger.error("Cannot delete nonexistent record.")
-            exit(1)
+            sys.exit()
     case _:
         raise ValueError(
             "Invalid parameter combination and/or values."
